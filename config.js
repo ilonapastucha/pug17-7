4d91f54807b8282df2c9b3f04128db91b5de85a4
Ilona Pastucha <ilonka.p4@gmail.com>
2018-10-23 17:00:31 +0200

    Add task files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.export = {
+    'GOOGLE_CLIENT_ID': '338298171497-jk7fi8s32pqrq0hnbgeq0dprp1tqkp7n.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'Cu3XoY92SicNYuTQ-0orHBia',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
+var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+
+function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+    };
+    cb(null, profile);
+}
+));
+
+app.set('view engine', 'pug'); //będziemy używać Puga jako kreatora widoków
+app.set('views','./views'); //widoki będziemy trzymać w katalogu /views
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', function (req, res) {
+    res.render('main', { user: req.user });
+});
+
+app.get('/signin', function (req, res) {
+    res.render('signin');
+});
+
+app.get('/log', function (req, res) {
+    res.render('log', {
+		first_name: req.query.first_name,
+        last_name: req.query.last_name,
+        user: googleProfile
+    });
+    
+});
+
+app.get('/auth/google',
+	passport.authenticate('google', {
+		scope : ['profile', 'email']
+}));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/log',
+        failureRedirect: '/'
+}));
+
+const server = app.listen(3000, 'localhost', function() {
+	const host = server.address().address;
+	const port = server.address().port;
+
+});
